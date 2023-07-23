57c83a09ad80d3db9afd49d719e8384c4b397511
tom-englert <mail@tom-englert.de>
2023-07-23 08:21:06 +0200

    Introduce Directory.Build.props, generate assembly info, replace file-patching of version with MSBuild logic

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("NuGetMonitor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuGetMonitor")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Tests")]
 
