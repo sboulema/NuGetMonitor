7afd2815fd07dac066a6ffea4213fa2e7dada8ba
tom-englert <mail@tom-englert.de>
2023-08-12 15:28:14 +0200

    fix loading issues

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using Microsoft.VisualStudio.Shell;
 [assembly: InternalsVisibleTo("Tests")]
 
 [assembly: ProvideCodeBase(AssemblyName = "DataGridExtensions")]
+[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Xaml.Behaviors")]
 [assembly: ProvideCodeBase(AssemblyName = "TomsToolbox.Essentials")]
 [assembly: ProvideCodeBase(AssemblyName = "TomsToolbox.Wpf")]
 [assembly: ProvideCodeBase(AssemblyName = "TomsToolbox.Wpf.Styles")]
